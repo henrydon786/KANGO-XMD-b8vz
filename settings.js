bcbcbae66f02b2d49ce4a20e7793e4285f051136
HENRY DON 🖤🔥 <lukassashy@gmail.com>
2025-06-26 13:20:18 +0530

    Update settings.js

--- settings.js
+++ settings.js
@@ -24,7 +24,7 @@ global.botname = process.env.BOT_NAME || 'HENRY-XMD'
 
 //-----------------[ OWNER NUMBER ]------------------//
 
-global.ownernumber = process.env.OWNER_NUMBER || '919235741670' 
+global.ownernumber = process.env.OWNER_NUMBER || '919451580236' 
 
 //--------------------[ SUDO ]--------------------------//
 
